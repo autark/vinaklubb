ed1917fd0ebea5996096e93015cf3f1833cb16dd
mongipong <42771549+mongipong@users.noreply.github.com>
2020-03-26 14:49:31 +0100

    Add files via upload

--- src/Video.js
+++ src/Video.js
@@ -8,36 +8,36 @@ export const Video = () => {
             <h2>Video</h2>
             <div className="row row-spacer">
                 <div className="col-md-4">                   
-                    <ReactPlayer height= "15rem"  width ="30rem" url="https://www.youtube.com/watch?v=up7tD_-yKoA" controls={true} />    
+                    <ReactPlayer height= "15rem"  width ="20rem" url="https://www.youtube.com/watch?v=up7tD_-yKoA" controls={true} />    
                     &nbsp;              
                 </div>
 
                 <div className="col-md-4">                   
-                    <ReactPlayer   height= "15rem"  width ="30rem"  url="https://www.youtube.com/watch?v=abb_DSEgrB4&t=681s" controls={true} />  
+                    <ReactPlayer   height= "15rem"  width ="20rem"  url="https://www.youtube.com/watch?v=abb_DSEgrB4&t=681s" controls={true} />  
                     &nbsp;              
                 </div>
 
                 <div className="col-md-4">
-                    <ReactPlayer   height= "15rem"  width ="30rem"  url="https://www.youtube.com/watch?v=tcN8UjxMHJc" controls={true} />   
+                    <ReactPlayer   height= "15rem"  width ="20rem"  url="https://www.youtube.com/watch?v=tcN8UjxMHJc" controls={true} />   
                     &nbsp;               
                 </div>
 
                 <div className="col-md-4">
-                    <ReactPlayer   height= "15rem"  width ="30rem"  url="https://www.youtube.com/watch?v=LBpkFFxWYGg&t=1086s" controls={true} /> 
+                    <ReactPlayer   height= "15rem"  width ="20rem"  url="https://www.youtube.com/watch?v=LBpkFFxWYGg&t=1086s" controls={true} /> 
                     &nbsp;                  
                 </div>
 
                 <div  className="col-md-4">
-                    <ReactPlayer  height= "15rem"  width ="30rem"  url="https://www.youtube.com/watch?v=ebeQHsE0Lng" controls={true} />   
+                    <ReactPlayer  height= "15rem"  width ="20rem"  url="https://www.youtube.com/watch?v=ebeQHsE0Lng" controls={true} />   
                     &nbsp;               
                 </div>
 
                 <div className="col-md-4">
-                    <ReactPlayer  height= "15rem"  width ="30rem"  url="https://www.youtube.com/watch?v=4QZkAqY3SwE" controls={true} />  
+                    <ReactPlayer  height= "15rem"  width ="20rem"  url="https://www.youtube.com/watch?v=4QZkAqY3SwE" controls={true} />  
                     &nbsp;                 
                 </div>
                 <div className="col-md-4">                   
-                    <ReactPlayer   height= "15rem"  width ="30rem"  url=" https://www.youtube.com/watch?v=i6IUgBgmTws&t=444s" controls={true} />
+                    <ReactPlayer   height= "15rem"  width ="20rem"  url=" https://www.youtube.com/watch?v=i6IUgBgmTws&t=444s" controls={true} />
                     &nbsp;
                 </div>
             </div>
